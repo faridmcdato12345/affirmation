f645728711799f8b803afd99da8b4ddf63b0b147
Adam Toth <adamjmtoth@gmail.com>
2023-04-12 02:33:11 +0000

    small fixes

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -21,8 +21,8 @@
 	@if(Auth::check())
 	<!-- header and footer bar go here-->
 	<div class="header header-fixed header-logo-center">
-		<a href="index.html" class="header-title">{{ env('APP_NAME') }}</a>
-		<a href="#" class="back-button header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
+		<a href="/" class="header-title">{{ env('APP_NAME') }}</a>
+		<a href="/" class="back-button header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
 		<a href="#" data-toggle-theme class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
 	</div>
 	<div id="footer-bar" class="footer-bar-1">

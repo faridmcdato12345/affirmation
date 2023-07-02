82df710a48ee2038b0f61d977ad927e4a96c5b59
Yvan Sabay <sabayyvan2018@gmail.com>
2023-07-03 02:55:11 +0800

    Added loading state

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -3,15 +3,22 @@
     :is="componentType == 'link' ? Link : 'button'"
     :href="href"
     :type="type"
+    :disabled="loading"
     :class="[
-      'duration-150 ease-out text-white text-center',
+      'duration-150 ease-out text-white text-center flex',
       btnSize, btnColor,
       {
         'w-full block': btnBlock
       },
       rounded ? 'rounded-full' : 'rounded-lg'
     ]">
-    {{ text }}
+    <span class="flex mx-auto">
+      <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+      </svg>
+      {{ text }}
+    </span>
   </component>
 </template>
 <script setup>
@@ -59,6 +66,10 @@ const props = defineProps({
   type: {
     type: String,
     default: 'button'
+  },
+  loading: {
+    type: Boolean,
+    default: false
   }
 })
 
@@ -80,6 +91,6 @@ const text = computed(() => props.uppercase ? props.label.toUpperCase() : props.
 const btnSize = computed(() => btnSizeClass[props.size])
 const btnColor = computed(() => {
   if(props.outline) return `border-2 border-${btnColorClass[props.color]}-600 border-solid`
-  return `bg-${btnColorClass[props.color]}-600 hover:bg-${btnColorClass[props.color]}-700 active:bg-${btnColorClass[props.color]}-800 focus:ring focus:ring-${btnColorClass[props.color]}-600`
+  return `bg-${btnColorClass[props.color]}-600 hover:bg-${btnColorClass[props.color]}-700 active:bg-${btnColorClass[props.color]}-800 disabled:hover:bg-${btnColorClass[props.color]}-600 focus:ring focus:ring-${btnColorClass[props.color]}-600`
 })
 </script>

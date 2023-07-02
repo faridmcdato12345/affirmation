f7b04290f339d1d3b202a6dd1c0f4224dab7a369
Yvan Sabay <sabayyvan2018@gmail.com>
2023-07-03 02:56:59 +0800

    Added own category option on category page

--- resources/js/Pages/Categories.vue
+++ resources/js/Pages/Categories.vue
@@ -6,6 +6,23 @@
       <h1 class="text-white text-3xl mt-8 text-center">
         Categories
       </h1>
+      <div class="flex flex-col w-full pb-2">
+        <h2 class="text-white font-medium text-2xl mt-8">
+          My Categories
+        </h2>
+        <hr />
+        <div class="bg-white relative hover:-translate-y-1 active:bg-gray-200 duration-200 ease-out w-96 rounded-md shadow px-4 py-4 cursor-pointer" @click.prevent="addCategoryModal = true">
+          <div class="absolute top-3 right-3">
+            <PlusCircleIcon class="w-6 text-green-600 hover:text-green-700" />
+          </div>
+          <h3 class="font-medium">
+            Add Category
+          </h3>
+          <p class="text-gray-600 text-sm">
+            Create your own category to be able to add your custom affirmations
+          </p>
+        </div>
+      </div>
       <div class="flex flex-col w-full pb-40">
         <div v-for="(categoryType, i) in categories" :key="`${i}-category`">
           <h2 class="text-white font-medium text-2xl mt-8">
@@ -13,13 +30,13 @@
           </h2>
           <hr />
           <div class="flex gap-4 flex-wrap">
-            <div 
-              v-for="category in categoryType" 
-              :key="category.id" 
+            <div
+              v-for="category in categoryType"
+              :key="category.id"
               class="relative hover:-translate-y-1 active:bg-gray-200 duration-200 ease-out w-96 rounded-md shadow px-4 py-4 cursor-pointer"
               :class="[
                 i == 0 || isPremium ? 'bg-white' : 'bg-gray-200'
-              ]" 
+              ]"
               @click.prevent="toggleSwitchCategory(category)">
               <div v-if="category.id === activeCategory" class="absolute right-3 top-3">
                 <CheckCircleIcon class="w-6 text-green-600" />
@@ -73,26 +90,31 @@
         <Button component-type="link" href="/billing" label="Subscribe for Access" color="success" />
       </div>
     </Modal>
-  </AuthenticatedLayout> 
+
+    <AddCategory v-model="addCategoryModal" />
+  </AuthenticatedLayout>
 </template>
 <script setup>
 import { ref } from 'vue'
-import { CheckCircleIcon, LockClosedIcon } from '@heroicons/vue/24/solid'
+import { CheckCircleIcon, LockClosedIcon, PlusCircleIcon } from '@heroicons/vue/24/solid'
 import { Head, router } from '@inertiajs/vue3'
 
 import AuthenticatedLayout from '../Layouts/AuthenticatedLayout.vue'
+import AddCategory from '../Components/Category/AddCategory.vue'
 import Modal from '../Components/Modal.vue'
 import Button from '../Components/Button.vue'
 
 const props = defineProps({
   categories: Object,
   activeCategory: [Object, Number],
-  isPremium: Boolean
+  isPremium: Boolean,
+  myCategory: Object
 })
 
 const selectedCategory = ref('')
 const setCategoryModal = ref(false)
 const upgradeModal = ref(false)
+const addCategoryModal = ref(true)
 
 const toggleSwitchCategory = (category) => {
   if(props.isPremium) {

e6222ba079ed753fc535e6361dab6901a67a404d
Yvan Sabay <sabayyvan2018@gmail.com>
2023-07-03 16:36:33 +0800

    custom affirmation initial setup

---
+++ resources/js/Components/Category/Affirmation/AddAffirmation.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Modal v-model="modalShown">
+      <div class="py-2">
+        <h1 class="">
+          Custom Affirmation
+        </h1>
+        <p class="text-base mt-2 font-light">
+          Add your own custom affirmation for your selected category.
+        </p>
+        <form class="mt-3" @submit.prevent="saveCategory">
+          <FormInput id="text" v-model="form.text" class="mb-1" label="Affirmation" :error="form.errors.text" />
+          <div class="flex justify-end gap-x-2 mt-3">
+            <Button label="Cancel" color="gray" @click.prevent="modalShown = false" />
+            <Button label="Save Affirmation" color="success" type="submit" :loading="loading" />
+          </div>
+        </form>
+      </div>
+    </Modal>
+  </div>
+</template>
+<script setup>
+import { computed, ref } from 'vue'
+// import { useForm } from '@inertiajs/vue3'
+
+// import { toast } from '../../Composables/useToast'
+
+import Modal from '../Modal.vue'
+import FormInput from '../FormInput.vue'
+import Button from '../Button.vue'
+
+const props = defineProps({
+  modelValue: Boolean
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const modalShown = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  }
+})
+
+const loading = ref(false)
+
+</script>
---
+++ resources/js/Components/Category/Affirmation/Affirmations.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Modal v-model="modalShown">
+      <div class="py-2">
+        <h1 class="">
+          Affirmations
+        </h1>
+        <p class="text-base mt-2 font-light">
+          Here are your custom affirmations for the selected category
+        </p>
+        <div class="max-h-[300px] overflow-y-auto">
+          <div class="relative flex justify-between px-3 py-3 border rounded-sm my-2">
+            <div>
+              <h2 class="font-medium">
+                Affirmation Title
+              </h2>
+              <p class="text-base text-gray-500">
+                Affirmation Description
+              </p>
+            </div>
+            <div class="absolute top-3 right-3 flex gap-x-1">
+              <PencilSquareIcon class="w-5 text-gray-500 hover:text-green-600 cursor-pointer" />
+              <TrashIcon class="w-5 text-gray-500 hover:text-red-600 cursor-pointer" />
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-end mt-4 gap-x-2">
+          <Button label="Close" color="gray" @click.prevent="emit('update:modelValue', false)" />
+          <Button label="Add Affirmation" color="success" @click.prevent="emit('update:modelValue', false)" />
+        </div>
+      </div>
+    </Modal>
+  </div>
+</template>
+<script setup>
+import { computed  } from 'vue'
+//   import { router } from '@inertiajs/vue3'
+//   import { toast } from '../../Composables/useToast'
+
+import Modal from '../../Modal.vue'
+import Button from '../../Button.vue'
+import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/solid'
+
+const props = defineProps({
+  modelValue: Boolean,
+  affirmations: {
+    type: Object,
+    default: () => {}
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const modalShown = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emit('update:modelValue', val)
+  }
+})
+
+// const loading = ref(false)
+</script>

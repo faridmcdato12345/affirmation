69da0ec21a54c0d316a65c8f5f63ea21311d2291
Yvan Sabay <sabayyvan2018@gmail.com>
2023-07-03 16:34:50 +0800

    Added explicit route model binding for usercategory

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\UserCategories;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -26,6 +27,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::model('user_category', UserCategories::class);
         $this->configureRateLimiting();
 
         $this->routes(function () {

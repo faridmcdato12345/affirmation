f740192b91b8a12d7519e67b2cc15d4f66b089d4
Farid <faridmcdato@gmail.com>
2023-08-21 05:03:50 +0800

    polished the ui for reminder function

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,9 +16,9 @@ const app = firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   // Customize notification here
-  const notificationTitle = 'Affirm';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
+    body: payload.notification.body,
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });

8e92ef803d5e5ad8d9bcdcdfe6bf099ab21ca91e
Farid <faridmcdato@gmail.com>
2023-08-28 00:22:08 +0800

    created a composable file for inserting data to indexdb

--- resources/js/Composables/useInsertIndexdb.js
+++ resources/js/Composables/useInsertIndexdb.js
@@ -72,63 +72,6 @@ export function useInsertIndexdb() {
     request.onerror = (event) => {
       console.error('Error opening database:', event.target.error)
     }
-    // console.log('opendatabase db: ',db)
-    // if (db) {
-    //   const transaction = db.transaction('user', 'readwrite')
-    //   const store = transaction.objectStore('user')
-    //   //   store.add({
-    //   //     id: 1,
-    //   //     user_id: userId
-    //   //   })
-    //   const getRequest = store.get(1)
-    //   getRequest.onsuccess = (event) => {
-    //     const existingData = event.target.result
-
-    //     if(existingData) {
-    //       existingData.user_id = userId
-    //       const putRequest = store.put(existingData)
-                
-    //       putRequest.onsuccess = () => {
-    //         console.log('Data updated successfully')
-    //       }
-    //       putRequest.onerror = (event) => {
-    //         console.error('Error updateing data: ', event.target.error)
-    //       }
-    //     } else {
-    //       store.add({
-    //         id: 1,
-    //         user_id: userId
-    //       })
-    //     }
-    //   }
-    //   transaction.oncomplete = () => {
-    //     console.log('userid inserted to indexdb')
-    //   }
-    // }
   }
-
-  //   const openDatabase = async () => {
-  //     return new Promise((resolve, reject) => {
-  //       const request = indexedDB.open('user_info', 2)
-
-  //       request.onsuccess = (event) => {
-  //         const db = event.target.result
-  //         resolve(db)
-  //       }
-
-  //       request.onerror = (event) => {
-  //         console.log('Error opening database: ', event.target.error)
-  //         reject()
-  //       }
-
-  //       request.onupgradeneeded = (event) => {
-  //         const db = event.target.result
-
-  //         if(!db.objectStoreNames.contains('user')){
-  //           db.createObjectStore('user', { keyPath: 'id',autoIncrement: false})
-  //         }
-  //       }
-  //     })
-  //   }
   return { insertData }
 }

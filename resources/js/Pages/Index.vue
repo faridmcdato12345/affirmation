0d9006cf1cbcc8f30cae13fbf4895a93e325eebf
Farid <faridmcdato@gmail.com>
2023-10-21 18:28:58 +0800

    refactor exercise result controller

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -82,7 +82,7 @@ const props = defineProps({
   isNotify: [Boolean, Number],
   userId: Number
 })
-
+console.log(props.exerciseFinished)
 const modalShown = ref(false)
 const completeModalShown = ref(false)
 const wrapper = ref('')

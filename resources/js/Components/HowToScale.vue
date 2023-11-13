afeb1718b2da867777f18d9a594ed2d64b31c2f3
Farid <faridmcdato@gmail.com>
2023-11-14 05:00:59 +0800

    added tooltip

---
+++ resources/js/Components/HowToScale.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1 class="font-medium text-2xl text-white">
+      What to do?
+    </h1>
+    <HowToScaleFirst v-if="step == 1" />
+    <HowToScaleSecond v-if="step == 2" />
+    <Button 
+      :label="step < 2 ? 'Next' : 'GOT IT!'" 
+      btn-block 
+      :darken="step >= 2"
+      class="mt-4" 
+      color="success"
+      @click.prevent="nextStep" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Button from './Button.vue'
+import HowToScaleFirst from './HowToScaleFirst.vue'
+import HowToScaleSecond from './HowToScaleSecond.vue'
+
+
+const step = ref(1)
+
+const nextStep = () => {
+  if(step.value == 2) {
+    emit('close-tooltip')
+  }
+
+  if(step.value < 2) step.value++ 
+} 
+
+const emit = defineEmits('close-tooltip')
+</script>
+
+<style scoped>
+.nested-list{
+    padding-left: 5%;
+}
+</style>
---
+++ resources/js/Components/HowToScaleSecond.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <p>3. Rate the affirmation if it fits what you feel today.</p>
+    <div class="l:grid grid-cols-2 gap-2 star-container sm:grid-cols-1 md:rid-cols-1">
+      <div class="flex  px-2 mb-2">
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <p>Very little</p>
+      </div>
+      <div class="flex  px-2 mb-2">
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <p>Little</p>
+      </div>
+      <div class="flex  px-2 mb-2">
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <p>Good</p>
+      </div>
+      <div class="flex  px-2 mb-2">
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <p>Very good</p>
+      </div>
+      <div class="flex  px-2 mb-2">
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <img src="images/svg/star.svg" alt="star-svg" class="star-icon" />
+        <p>Great</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.star-icon{
+  width: 30px;
+  height: 30px;
+}
+p{
+  color:#fff;
+  
+}
+.star-container .flex p{
+  margin-left: 20px;
+}
+</style>
